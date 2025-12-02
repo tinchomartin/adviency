c5904ce16387c34f83915eb94bbe5344365b8610
Martin Garay <48074105+tinchomartin@users.noreply.github.com>
2025-12-02 11:05:10 -0300

    update fix colores

--- src/components/GiftList/index.js
+++ src/components/GiftList/index.js
@@ -9,10 +9,10 @@ function GiftList(props) {
     <Box display="grid" gridTemplateColumns={"50px auto"} gridGap={2}>
       <Image src={gift.image} alt="Image's gift"></Image>
       <Box display="grid" gridTemplateRows={"auto auto"}>
-        <Box as="p" fontSize={24}>
+        <Box as="p" color="black" fontSize={24}>
           {gift.type}
         </Box>
-        <Box as="p" fontSize={12} textAlign="left">
+        <Box as="p" color="black" fontSize={12} textAlign="left">
           {gift.receiver}
         </Box>
       </Box>
